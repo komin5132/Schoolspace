fde9711f6498e49cafc7253a39705779f0820dcb
Komin <Kominowskikacper@gmail.com>
2026-03-04 19:54:17 +0100

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
+    lint {
+        disable.add("MissingDefaultResource")
+    }
 }
 kotlin {
     jvmToolchain(17)
